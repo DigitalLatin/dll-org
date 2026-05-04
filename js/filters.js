8b3bf935344898986997edaa35655fe4810342df
sjhuskey <samueljhuskey@gmail.com>
2026-05-04 20:27:58 +0000

    Deploying to gh-pages from @ DigitalLatin/dll-org@325ef18fb49c2c239bd21abce74c9b25883a1f00 🚀

--- js/filters.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  readableDate: (dateString) => {
-    const date = new Date(dateString);
-    const options = { year: "numeric", month: "long", day: "numeric" };
-    return date.toLocaleDateString("en-US", options);
-  },
-};
